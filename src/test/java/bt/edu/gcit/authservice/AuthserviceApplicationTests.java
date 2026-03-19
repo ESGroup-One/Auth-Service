96a4bb059fc706e3be768840afdb50faefaee502
Lungten <12230028.gcit@rub.edu.bt>
2026-03-19 10:50:31 +0000

    added initial files

---
+++ src/test/java/bt/edu/gcit/authservice/AuthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package bt.edu.gcit.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
